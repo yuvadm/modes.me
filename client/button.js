81dd4c59c6d51d58e16937a49bfb2068a28d4c99
Yuval Adam <yuval@y3xz.com>
2015-06-20 00:58:56 +0300

    Dates screen

--- client/button.js
+++ client/button.js
@@ -124,10 +124,9 @@ Template.dates.events({
 
 Template.dates.helpers({
   months: [
-    [{val: 1, name: 'Jan.'}, {val: 2, name: 'Feb.'}, {val: 3, name: 'March'}],
-    [{val: 4, name: 'April'}, {val: 5, name: 'May'}, {val: 6, name: 'June'}],
-    [{val: 7, name: 'July'}, {val: 8, name: 'Aug.'}, {val: 9, name: 'Sept.'}],
-    [{val: 10, name: 'Oct.'}, {val: 11, name: 'Nov.'}, {val: 12, name: 'Dec.'}],
+    [{val: 1, name: 'January'}, {val: 2, name: 'February'}, {val: 3, name: 'March'}, {val: 4, name: 'April'}],
+    [{val: 5, name: 'May'}, {val: 6, name: 'June'}, {val: 7, name: 'July'}, {val: 8, name: 'August'}],
+    [{val: 9, name: 'September'}, {val: 10, name: 'October'}, {val: 11, name: 'November'}, {val: 12, name: 'December'}],
   ],
   years: _.range(2012, 2016)
 })
