551dd70061eb8c1bf6adb958848395bbd55b9a7f
Yuval Adam <yuval@y3xz.com>
2015-06-30 17:28:20 +0300

    Fix dominants hex values

--- client/button.js
+++ client/button.js
@@ -55,12 +55,12 @@ function animate() {
     $('div#dominant1').css('background', 'rgba(' + palette[0][0] + ',' + palette[0][1] + ',' + palette[0][2] + ',1)');
     $('div#dominant2').css('background', 'rgba(' + palette[1][0] + ',' + palette[1][1] + ',' + palette[1][2] + ',1)');
 
-    $('div#dominant1').siblings('h5').text('#'
+    $('h5#dominant1').text('#'
       + palette[0][0].toString(16).toUpperCase()
       + palette[0][1].toString(16).toUpperCase()
       + palette[0][2].toString(16).toUpperCase()
     )
-    $('div#dominant2').siblings('h5').text('#'
+    $('h5#dominant2').text('#'
       + palette[1][0].toString(16).toUpperCase()
       + palette[1][1].toString(16).toUpperCase()
       + palette[1][2].toString(16).toUpperCase()
